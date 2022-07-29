74aeabdbfc1261a3a4c29e81f67011087020ee78
Pierre Fenoll <pierrefenoll@gmail.com>
2022-07-29 23:00:47 +0200

    fix test
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -69,11 +69,19 @@ fn test_bad_job_zero() {
 
     let err = format!("{}", "NaN".parse::<u32>().unwrap_err());
 
+    let ixs_failed = try_quick(&xs, 0, |subxs| {
+        if !subxs.contains(&0) {
+            Ok(())
+        } else {
+            Err(err.clone())
+        }
+    });
+    assert_eq!(ixs_failed, Err(err.clone()));
+
     use std::ops::RangeInclusive as RI;
     let errors = |range: RI<usize>| range.map(|v| (v, err.clone())).collect::<HashMap<_, _>>();
 
     for (d, errors) in [
-        (0, errors(0..=99)),
         (1, errors(0..=49)),
         (2, errors(0..=24)),
         (3, errors(0..=11)),
