7e075e3414f8d55bb48f057292a85dcfc51a4121
Pierre Fenoll <pierrefenoll@gmail.com>
2023-07-22 18:43:04 +0200

    fixes
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -15,31 +15,31 @@ type Res<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync + '
 // CliArgs correspond to `docker buildx bake --help`
 struct CliArgs {
     /// Read commands from file.
-    #[clap(short, long="file", default_value_t=String::from("-"))]
+    #[arg(short, long="file", default_value_t=String::from("-"))]
     file: String,
 
     /// Do not use cache when building the image
-    #[clap(long = "no-cache")]
+    #[arg(long = "no-cache")]
     no_cache: bool,
 
     /// Print the options without building
-    #[clap(long = "print")]
+    #[arg(long = "print")]
     print: bool,
 
     /// Set type of progress output ("plain", "tty")
-    #[clap(long="progress", default_value_t=String::from("auto"))]
+    #[arg(long="progress", default_value_t=String::from("auto"))]
     progress: String,
 
     /// Always attempt to pull all referenced images
-    #[clap(long = "pull")]
+    #[arg(long = "pull")]
     pull: bool,
 
     /// Print more things
-    #[clap(long = "debug")]
+    #[arg(long = "debug")]
     debug: bool,
 
     /// Retry each failed build at most this many times
-    #[clap(long = "retry", default_value_t = 3u8)]
+    #[arg(long = "retry", default_value_t = 3u8)]
     retry: u8,
 }
 
@@ -276,67 +276,67 @@ struct BuildArgs {
     path_or_url: String, // context
 
     /// Set build-time variables
-    #[clap(long = "build-arg")]
+    #[arg(long = "build-arg")]
     build_args: Vec<String>, // args
 
     /// Additional build contexts (e.g., name=path)
-    #[clap(long = "build-context")]
+    #[arg(long = "build-context")]
     build_context: Option<String>, // contexts (TODO: stringArray)
 
     /// External cache sources
-    #[clap(long = "cache-from")]
+    #[arg(long = "cache-from")]
     cache_from: Option<String>, // cache-from (TODO: stringArray)
 
     /// Cache export destinations
-    #[clap(long = "cache-to")]
+    #[arg(long = "cache-to")]
     cache_to: Option<String>, // cache-to (TODO: stringArray)
 
     /// Name of the Dockerfile
-    #[clap(short, long = "file")]
+    #[arg(short, long = "file")]
     file: Option<String>, // dockerfile
 
     /// Set metadata for an image
-    #[clap(long = "label")]
+    #[arg(long = "label")]
     label: Option<String>, // labels (TODO: stringArray)
 
     // FIXME: https://docs.rs/clap/latest/clap/enum.ArgAction.html#variant.Append
     /// Set the networking mode for the "RUN" instructions during build (default "default")
-    #[clap(long = "network", action(clap::ArgAction::Append))]
+    #[arg(long = "network", action(clap::ArgAction::Append))]
     network: Option<String>,
 
     /// Do not use cache when building the image
-    #[clap(long = "no-cache")]
+    #[arg(long = "no-cache")]
     no_cache: bool, // no-cache
 
     /// Do not cache specified stages
-    #[clap(long = "no-cache-filter")]
+    #[arg(long = "no-cache-filter")]
     no_cache_filter: Option<String>, // no-cache-filter (TODO: stringArray)
 
     /// Output destination (format: "type=local,dest=path")
-    #[clap(short, long = "output")]
+    #[arg(short, long = "output")]
     output: Option<String>, // output (TODO: stringArray)
 
     /// Set target platform for build
-    #[clap(long = "platform")]
+    #[arg(long = "platform")]
     platform: Option<String>, // platforms (TODO: stringArray)
 
     /// Always attempt to pull all referenced images
-    #[clap(long = "pull")]
+    #[arg(long = "pull")]
     pull: bool, // pull
 
     /// Secret to expose to the build (format: "id=mysecret[,src=/local/secret]")
-    #[clap(long = "push")]
+    #[arg(long = "push")]
     secret: Option<String>, // secrets (TOOD: stringArray)
 
     /// SSH agent socket or keys to expose to the build (format: "default|<id>[=<socket>|<key>[,<key>]]")
-    #[clap(long = "ssh")]
+    #[arg(long = "ssh")]
     ssh: Option<String>, // ssh (TODO: stringArray)
 
     /// Name and optionally a tag (format: "name:tag")
-    #[clap(short, long = "tag")]
+    #[arg(short, long = "tag")]
     tag: Option<String>, // tags (TODO: stringArray)
 
     /// Set the target build stage to build
-    #[clap(long = "target")]
+    #[arg(long = "target")]
     target: Option<String>, // target
 }
