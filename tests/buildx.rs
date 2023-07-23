46a8d6a4747cfe0cceffa8d3c14470b22d21b088
Pierre Fenoll <pierrefenoll@gmail.com>
2023-07-23 15:02:51 +0200

    fix fmt
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>

--- tests/buildx.rs
+++ tests/buildx.rs
@@ -211,23 +211,15 @@ Usage:  docker [OPTIONS] COMMAND
 A self-sufficient runtime for containers
 
 Options:
-      --config string      Location of client config files (default
-                           "/home/runner/.docker")
-  -c, --context string     Name of the context to use to connect to the
-                           daemon (overrides DOCKER_HOST env var and
-                           default context set with "docker context use")
+      --config string      Location of client config files (default "~/.docker")
+  -c, --context string     Name of the context to use to connect to the daemon (overrides DOCKER_HOST env var and default context set with "docker context use")
   -D, --debug              Enable debug mode
   -H, --host list          Daemon socket(s) to connect to
-  -l, --log-level string   Set the logging level
-                           ("debug"|"info"|"warn"|"error"|"fatal")
-                           (default "info")
+  -l, --log-level string   Set the logging level ("debug"|"info"|"warn"|"error"|"fatal") (default "info")
       --tls                Use TLS; implied by --tlsverify
-      --tlscacert string   Trust certs signed only by this CA (default
-                           "/home/runner/.docker/ca.pem")
-      --tlscert string     Path to TLS certificate file (default
-                           "/home/runner/.docker/cert.pem")
-      --tlskey string      Path to TLS key file (default
-                           "/home/runner/.docker/key.pem")
+      --tlscacert string   Trust certs signed only by this CA (default "~/.docker/ca.pem")
+      --tlscert string     Path to TLS certificate file (default "~/.docker/cert.pem")
+      --tlskey string      Path to TLS key file (default "~/.docker/key.pem")
       --tlsverify          Use TLS and verify the remote
   -v, --version            Print version information and quit
 
@@ -312,9 +304,7 @@ Options:
       --metadata-file string   Write build result metadata to the file
       --no-cache               Do not use cache when building the image
       --print                  Print the options without building
-      --progress string        Set type of progress output ("auto",
-                               "plain", "tty"). Use plain to show
-                               container output (default "auto")
+      --progress string        Set type of progress output ("auto", "plain", "tty"). Use plain to show container output (default "auto")
       --provenance string      Shorthand for "--set=*.attest=type=provenance"
       --pull                   Always attempt to pull all referenced images
       --push                   Shorthand for "--set=*.output=type=registry"
@@ -331,58 +321,35 @@ Aliases:
   docker buildx build, docker buildx b
 
 Options:
-      --add-host strings              Add a custom host-to-IP mapping
-                                      (format: "host:ip")
-      --allow strings                 Allow extra privileged entitlement
-                                      (e.g., "network.host",
-                                      "security.insecure")
-      --attest stringArray            Attestation parameters (format:
-                                      "type=sbom,generator=image")
+      --add-host strings              Add a custom host-to-IP mapping (format: "host:ip")
+      --allow strings                 Allow extra privileged entitlement (e.g., "network.host", "security.insecure")
+      --attest stringArray            Attestation parameters (format: "type=sbom,generator=image")
       --build-arg stringArray         Set build-time variables
-      --build-context stringArray     Additional build contexts (e.g.,
-                                      name=path)
+      --build-context stringArray     Additional build contexts (e.g., name=path)
       --builder string                Override the configured builder instance
-      --cache-from stringArray        External cache sources (e.g.,
-                                      "user/app:cache",
-                                      "type=local,src=path/to/dir")
-      --cache-to stringArray          Cache export destinations (e.g.,
-                                      "user/app:cache",
-                                      "type=local,dest=path/to/dir")
+      --cache-from stringArray        External cache sources (e.g., "user/app:cache", "type=local,src=path/to/dir")
+      --cache-to stringArray          Cache export destinations (e.g., "user/app:cache", "type=local,dest=path/to/dir")
       --cgroup-parent string          Optional parent cgroup for the container
-  -f, --file string                   Name of the Dockerfile (default:
-                                      "PATH/Dockerfile")
+  -f, --file string                   Name of the Dockerfile (default: "PATH/Dockerfile")
       --iidfile string                Write the image ID to the file
       --label stringArray             Set metadata for an image
       --load                          Shorthand for "--output=type=docker"
       --metadata-file string          Write build result metadata to the file
-      --network string                Set the networking mode for the
-                                      "RUN" instructions during build
-                                      (default "default")
+      --network string                Set the networking mode for the "RUN" instructions during build (default "default")
       --no-cache                      Do not use cache when building the image
       --no-cache-filter stringArray   Do not cache specified stages
-  -o, --output stringArray            Output destination (format:
-                                      "type=local,dest=path")
+  -o, --output stringArray            Output destination (format: "type=local,dest=path")
       --platform stringArray          Set target platform for build
-      --progress string               Set type of progress output
-                                      ("auto", "plain", "tty"). Use plain
-                                      to show container output (default
-                                      "auto")
+      --progress string               Set type of progress output ("auto", "plain", "tty"). Use plain to show container output (default "auto")
       --provenance string             Shorthand for "--attest=type=provenance"
-      --pull                          Always attempt to pull all
-                                      referenced images
+      --pull                          Always attempt to pull all referenced images
       --push                          Shorthand for "--output=type=registry"
-  -q, --quiet                         Suppress the build output and print
-                                      image ID on success
+  -q, --quiet                         Suppress the build output and print image ID on success
       --sbom string                   Shorthand for "--attest=type=sbom"
-      --secret stringArray            Secret to expose to the build
-                                      (format:
-                                      "id=mysecret[,src=/local/secret]")
+      --secret stringArray            Secret to expose to the build (format: "id=mysecret[,src=/local/secret]")
       --shm-size bytes                Size of "/dev/shm"
-      --ssh stringArray               SSH agent socket or keys to expose
-                                      to the build (format:
-                                      "default|<id>[=<socket>|<key>[,<key>]]")
-  -t, --tag stringArray               Name and optionally a tag (format:
-                                      "name:tag")
+      --ssh stringArray               SSH agent socket or keys to expose to the build (format: "default|<id>[=<socket>|<key>[,<key>]]")
+  -t, --tag stringArray               Name and optionally a tag (format: "name:tag")
       --target string                 Set the target build stage to build
       --ulimit ulimit                 Ulimit options (default [])
 "#,
