8cf5d32131885cf6cdd0898384f2e9c9371cdad8
Pierre Fenoll <pierrefenoll@gmail.com>
2023-08-15 12:53:19 +0200

    there
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>

--- tests/buildx.rs
+++ tests/buildx.rs
@@ -1,4 +1,4 @@
-use std::{assert_eq, process::Command};
+use std::{assert_eq, process::Command, unreachable};
 
 use predicates::{str::diff, Predicate};
 
@@ -54,18 +54,22 @@ fn cli_installed_docker_usage() {
 fn usages(version: &str) -> (String, &'static str, &'static str) {
     let short: String = version.replace('+', " ").split_ascii_whitespace().take(2).collect();
     match short.as_str() {
-        "github.com/docker/buildxv0.11.1" => {
-            (moby("v0.11.1", "v2.6.1"), DOCKER_BUILDX_BAKE, DOCKER_BUILDX_BUILD)
-        }
-        "github.com/docker/buildxv0.11.2" => {
-            (moby("v0.11.2", "v2.6.1"), DOCKER_BUILDX_BAKE, DOCKER_BUILDX_BUILD)
-        }
+        short @ "github.com/docker/buildxv0.11.1" => (
+            moby(short.strip_prefix("github.com/docker/buildxv").unwrap()),
+            DOCKER_BUILDX_BAKE,
+            DOCKER_BUILDX_BUILD,
+        ),
+        short @ "github.com/docker/buildxv0.11.2" => (
+            moby(short.strip_prefix("github.com/docker/buildxv").unwrap()),
+            DOCKER_BUILDX_BAKE,
+            DOCKER_BUILDX_BUILD,
+        ),
 
-        "github.com/docker/buildx0.11.1" => {
-            (azure("0.11.1+azure-1", "2.20.0+azure-1"), DOCKER_BUILDX_BAKE, DOCKER_BUILDX_BUILD)
+        short @ "github.com/docker/buildx0.11.1" => {
+            (azure(short), DOCKER_BUILDX_BAKE, DOCKER_BUILDX_BUILD)
         }
-        "github.com/docker/buildx0.11.2" => {
-            (azure("0.11.2+azure-1", "2.20.0+azure-1"), DOCKER_BUILDX_BAKE, DOCKER_BUILDX_BUILD)
+        short @ "github.com/docker/buildx0.11.2" => {
+            (azure(short), DOCKER_BUILDX_BAKE, DOCKER_BUILDX_BUILD)
         }
 
         _ => ("UNHANDLED".to_owned(), "UNHANDLED", "UNHANDLED"),
@@ -137,7 +141,7 @@ Options:
       --ulimit ulimit                 Ulimit options (default [])
 "#;
 
-fn moby(buildx: &str, compose: &str) -> String {
+fn moby(buildx: &str) -> String {
     format!(
         r#"
 Usage:  docker [OPTIONS] COMMAND
@@ -160,8 +164,8 @@ Common Commands:
 
 Management Commands:
   builder     Manage builds
-  buildx*     Docker Buildx (Docker Inc., {buildx})
-  compose*    Docker Compose (Docker Inc., {compose})
+  buildx*     Docker Buildx (Docker Inc., v{buildx})
+  compose*    Docker Compose (Docker Inc., v2.6.1)
   container   Manage containers
   context     Manage contexts
   image       Manage images
@@ -226,9 +230,10 @@ For more help on how to use Docker, head to https://docs.docker.com/go/guides/
     )
 }
 
-fn azure(buildx: &str, compose: &str) -> String {
-    format!(
-        r#"
+fn azure(short: &str) -> String {
+    match short {
+        "github.com/docker/buildx0.11.1" => {
+            r#"
 Usage:  docker [OPTIONS] COMMAND
 
 A self-sufficient runtime for containers
@@ -248,8 +253,8 @@ Options:
 
 Management Commands:
   builder     Manage builds
-  buildx*     Docker Buildx (Docker Inc., {buildx})
-  compose*    Docker Compose (Docker Inc., {compose})
+  buildx*     Docker Buildx (Docker Inc., 0.11.1+azure-1)
+  compose*    Docker Compose (Docker Inc., 2.20.0+azure-1)
   config      Manage Docker configs
   container   Manage containers
   context     Manage contexts
@@ -312,5 +317,94 @@ Run 'docker COMMAND --help' for more information on a command.
 
 To get more help with docker, check out our guides at https://docs.docker.com/go/guides/
 "#
-    )
+        }
+
+        "github.com/docker/buildx0.11.2" => {
+            r#"
+Usage:  docker [OPTIONS] COMMAND
+
+A self-sufficient runtime for containers
+
+Options:
+Common Commands:
+  run         Create and run a new container from an image
+  exec        Execute a command in a running container
+  ps          List containers
+  build       Build an image from a Dockerfile
+  pull        Download an image from a registry
+  push        Upload an image to a registry
+  images      List images
+  login       Log in to a registry
+  logout      Log out from a registry
+  search      Search Docker Hub for images
+  version     Show the Docker version information
+  info        Display system-wide information
+
+Management Commands:
+  builder     Manage builds
+  buildx*     Docker Buildx (Docker Inc., 0.11.2+azure-1)
+  compose*    Docker Compose (Docker Inc., 2.20.2+azure-1)
+  container   Manage containers
+  context     Manage contexts
+  image       Manage images
+  manifest    Manage Docker image manifests and manifest lists
+  network     Manage networks
+  plugin      Manage plugins
+  system      Manage Docker
+  trust       Manage trust on Docker images
+  volume      Manage volumes
+
+Swarm Commands:
+  swarm       Manage Swarm
+
+Commands:
+  attach      Attach local standard input, output, and error streams to a running container
+  commit      Create a new image from a container's changes
+  cp          Copy files/folders between a container and the local filesystem
+  create      Create a new container
+  diff        Inspect changes to files or directories on a container's filesystem
+  events      Get real time events from the server
+  export      Export a container's filesystem as a tar archive
+  history     Show the history of an image
+  import      Import the contents from a tarball to create a filesystem image
+  inspect     Return low-level information on Docker objects
+  kill        Kill one or more running containers
+  load        Load an image from a tar archive or STDIN
+  logs        Fetch the logs of a container
+  pause       Pause all processes within one or more containers
+  port        List port mappings or a specific mapping for the container
+  rename      Rename a container
+  restart     Restart one or more containers
+  rm          Remove one or more containers
+  rmi         Remove one or more images
+  save        Save one or more images to a tar archive (streamed to STDOUT by default)
+  start       Start one or more stopped containers
+  stats       Display a live stream of container(s) resource usage statistics
+  stop        Stop one or more running containers
+  tag         Create a tag TARGET_IMAGE that refers to SOURCE_IMAGE
+  top         Display the running processes of a container
+  unpause     Unpause all processes within one or more containers
+  update      Update configuration of one or more containers
+  wait        Block until one or more containers stop, then print their exit codes
+
+Global Options:
+      --config string      Location of client config files (default "~/.docker")
+  -c, --context string     Name of the context to use to connect to the daemon (overrides DOCKER_HOST env var and default context set with "docker context use")
+  -D, --debug              Enable debug mode
+  -H, --host list          Daemon socket(s) to connect to
+  -l, --log-level string   Set the logging level ("debug", "info", "warn", "error", "fatal") (default "info")
+      --tls                Use TLS; implied by --tlsverify
+      --tlscacert string   Trust certs signed only by this CA (default "~/.docker/ca.pem")
+      --tlscert string     Path to TLS certificate file (default "~/.docker/cert.pem")
+      --tlskey string      Path to TLS key file (default "~/.docker/key.pem")
+      --tlsverify          Use TLS and verify the remote
+  -v, --version            Print version information and quit
+
+Run 'docker COMMAND --help' for more information on a command.
+
+For more help on how to use Docker, head to https://docs.docker.com/go/guides/
+"#
+        }
+        _ => unreachable!("{short}"),
+    }.to_owned()
 }
