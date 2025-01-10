8f794aced1b710f81ac8b420fcfc809b445430a2
Pierre Fenoll <pierrefenoll@gmail.com>
2025-01-10 18:32:37 +0100

    update tests
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>

--- tests/buildx.rs
+++ tests/buildx.rs
@@ -70,7 +70,8 @@ Global Options:
 Run 'docker COMMAND --help' for more information on a command.
 "#;
 
-const DOCKER_BUILDX_BAKE: &str = r#"
+const DOCKER_BUILDX_BAKE: &str = r#"Build from a file
+
 Usage:  docker buildx bake [OPTIONS] [TARGET...]
 
 Build from a file
@@ -79,27 +80,34 @@ Aliases:
   docker buildx bake, docker buildx f
 
 Options:
+      --allow stringArray      Allow build to access specified resources
       --builder string         Override the configured builder instance
+      --call string            Set method for evaluating build ("check", "outline", "targets") (default "build")
+      --check                  Shorthand for "--call=check" (default )
+  -D, --debug                  Enable debug logging
   -f, --file stringArray       Build definition file
       --load                   Shorthand for "--set=*.output=type=docker"
-      --metadata-file string   Write build result metadata to the file
+      --metadata-file string   Write build result metadata to a file
       --no-cache               Do not use cache when building the image
       --print                  Print the options without building
-      --progress string        Set type of progress output ("auto", "plain", "tty"). Use plain to show container output (default "auto")
+      --progress string        Set type of progress output ("auto", "plain", "tty", "rawjson"). Use plain to show container output (default "auto")
       --provenance string      Shorthand for "--set=*.attest=type=provenance"
       --pull                   Always attempt to pull all referenced images
       --push                   Shorthand for "--set=*.output=type=registry"
       --sbom string            Shorthand for "--set=*.attest=type=sbom"
       --set stringArray        Override target value (e.g., "targetpattern.key=value")
+
+Experimental commands and flags are hidden. Set BUILDX_EXPERIMENTAL=1 to show them.
 "#;
 
-const DOCKER_BUILDX_BUILD: &str = r#"
+const DOCKER_BUILDX_BUILD: &str = r#"Start a build
+
 Usage:  docker buildx build [OPTIONS] PATH | URL | -
 
 Start a build
 
 Aliases:
-  docker buildx build, docker buildx b
+  docker build, docker builder build, docker image build, docker buildx b
 
 Options:
       --add-host strings              Add a custom host-to-IP mapping (format: "host:ip")
@@ -111,27 +119,32 @@ Options:
       --builder string                Override the configured builder instance
       --cache-from stringArray        External cache sources (e.g., "user/app:cache", "type=local,src=path/to/dir")
       --cache-to stringArray          Cache export destinations (e.g., "user/app:cache", "type=local,dest=path/to/dir")
+      --call string                   Set method for evaluating build ("check", "outline", "targets") (default "build")
       --cgroup-parent string          Set the parent cgroup for the "RUN" instructions during build
+      --check                         Shorthand for "--call=check" (default )
+  -D, --debug                         Enable debug logging
   -f, --file string                   Name of the Dockerfile (default: "PATH/Dockerfile")
-      --iidfile string                Write the image ID to the file
+      --iidfile string                Write the image ID to a file
       --label stringArray             Set metadata for an image
       --load                          Shorthand for "--output=type=docker"
-      --metadata-file string          Write build result metadata to the file
+      --metadata-file string          Write build result metadata to a file
       --network string                Set the networking mode for the "RUN" instructions during build (default "default")
       --no-cache                      Do not use cache when building the image
       --no-cache-filter stringArray   Do not cache specified stages
   -o, --output stringArray            Output destination (format: "type=local,dest=path")
       --platform stringArray          Set target platform for build
-      --progress string               Set type of progress output ("auto", "plain", "tty"). Use plain to show container output (default "auto")
+      --progress string               Set type of progress output ("auto", "plain", "tty", "rawjson"). Use plain to show container output (default "auto")
       --provenance string             Shorthand for "--attest=type=provenance"
       --pull                          Always attempt to pull all referenced images
       --push                          Shorthand for "--output=type=registry"
   -q, --quiet                         Suppress the build output and print image ID on success
       --sbom string                   Shorthand for "--attest=type=sbom"
       --secret stringArray            Secret to expose to the build (format: "id=mysecret[,src=/local/secret]")
-      --shm-size bytes                Size of "/dev/shm"
+      --shm-size bytes                Shared memory size for build containers
       --ssh stringArray               SSH agent socket or keys to expose to the build (format: "default|<id>[=<socket>|<key>[,<key>]]")
   -t, --tag stringArray               Name and optionally a tag (format: "name:tag")
       --target string                 Set the target build stage to build
       --ulimit ulimit                 Ulimit options (default [])
+
+Experimental commands and flags are hidden. Set BUILDX_EXPERIMENTAL=1 to show them.
 "#;
